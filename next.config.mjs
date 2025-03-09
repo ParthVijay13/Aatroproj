9c839892b5b858c65480cad3c4d2a137a9cad695
ParthVijay13 <104959222+ParthVijay13@users.noreply.github.com>
2025-03-09 23:43:57 +0530

    webpack mb size error resolved

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,17 @@ const nextConfig = {
     // experimental: {
     //     turbopack: false,
     //   },
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.optimization.splitChunks = {
+          chunks: 'all',
+          maxSize: 2000000, // 2MB per chunk
+        };
+      }
+      return config;
+    },
     images: {
-      domains: ['localhost','images.pexels.com'],
+      domains: ['localhost'],
     },
 };
 
